805761305d9d5d56de825eefc56099175d400d19
seanpaulkelley <sean@horse101.com>
2017-02-27 22:21:31 -0500

    added readings graph with sensor_type parameter

---
+++ frontend/app/controllers/readings.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+export default Ember.Controller.extend({
+  valueData: Ember.computed('model', function(){
+    
+    return {
+      labels: this.get('model').mapBy('created_at'),
+      datasets: [{
+        label: 'Readings',
+        data: this.get('model').mapBy('value')
+      }]
+    }
+  })
+})
--- frontend/app/routes/readings.js
+++ frontend/app/routes/readings.js
@@ -4,6 +4,11 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.store.findAll('reading');
+    // return this.store.findAll('reading');
+    
+    //return this.store.query('reading', { filter: { sensor_type: 'soil' } })
+    
+    return this.store.query('reading', {sensor_type: 'soil' });
+
   }
 });
