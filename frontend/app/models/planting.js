901b839418e817d851d37d8a5197e099ac4ca2a9
seanpaulkelley <sean@horse101.com>
2017-03-12 12:13:06 -0400

    resolve date issue.  added week filters to plantings

--- frontend/app/models/planting.js
+++ frontend/app/models/planting.js
@@ -3,8 +3,8 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   plant: DS.belongsTo('plant'),
   plantingType: DS.attr('string'),
-  plantingDateBegin: DS.attr('date'),
-  plantingDateEnd: DS.attr('date'),
+  plantingDateBegin: DS.attr('utc'),
+  plantingDateEnd: DS.attr('utc'),
   distanceBetweenFrom: DS.attr('number'),
   distanceBetweenTo: DS.attr('number'),
   daysToHarvestFrom: DS.attr('number'),
--- frontend/app/routes/plantings.js
+++ frontend/app/routes/plantings.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    queryParams: {
+    week: {
+      refreshModel: true
+    }
+  },
 
-  model() {
-    return this.get('store').findAll('planting',{include: 'plants'});
+  model(params) {
+    //alert(params.week)
+    //return this.get('store').findAll('planting',{include: 'plants'});
+    return this.store.query('planting', params, {include: 'plants'});
   }
 });
