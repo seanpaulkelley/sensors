27b950d5de316ca8b5ed13862d03eeedc9767deb
seanpaulkelley <sean@horse101.com>
2017-03-15 07:54:39 -0400

    format readings chart dates

--- frontend/app/controllers/readings.js
+++ frontend/app/controllers/readings.js
@@ -3,16 +3,32 @@ export default Ember.Controller.extend({
 
   queryParams: ['sensor_type'],
   sensor_type: 'soil',
+  prettyDate: Ember.computed('model', function(){
+    return this.get('model.created_at');
+  }),
 
   valueData: Ember.computed('model', function(){
-    
-    return {
+   return {
       labels: this.get('model').mapBy('created_at'),
       datasets: [{
         label: 'Readings',
         data: this.get('model').mapBy('value')
       }]
     };
-  })
+  }),
+   
+  lineOptions: {
+    scales: {
+            xAxes: [{
+                type: 'time',
+                time: {
+                    displayFormats: {
+                        hour:'MM/DD/YY HH:MM'
+                    }//,
+                //unit: 'minute'   
+                }
+            }]
+        }
+  }
 
 });
--- frontend/app/models/reading.js
+++ frontend/app/models/reading.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   sensor_type: DS.attr('string'),
   value: DS.attr('number'),
-  created_at: DS.attr('string'),
+  //created_at: DS.attr('string'),
+  created_at: DS.attr('string')
 });
