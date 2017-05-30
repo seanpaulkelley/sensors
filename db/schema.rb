3c8139b328c2b7327f347fc5bc6e46719a34a7d2
seanpaulkelley <sean@horse101.com>
2017-05-29 23:06:28 -0400

    added root cellar links to readings page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170312115212) do
+ActiveRecord::Schema.define(version: 20170418182806) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 20170312115212) do
     t.string  "name"
     t.integer "distance_between_from"
     t.integer "distance_between_to"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "readings", force: :cascade do |t|
