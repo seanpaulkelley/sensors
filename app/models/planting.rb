f5e0f88ef16e146c931750ba1e3ee72b2a035558
seanpaulkelley <sean@horse101.com>
2018-06-23 18:04:04 -0400

    compiled ember

--- app/models/planting.rb
+++ app/models/planting.rb
@@ -6,20 +6,20 @@ class Planting < ApplicationRecord
   validates :days_to_harvest_from, presence: true
 
   def self.this_week
-    start_date = Date.today.at_beginning_of_week
-    end_date = Date.today.at_end_of_week
+    start_date = Date.today.at_beginning_of_week.change(:year => 2017)
+    end_date = Date.today.at_end_of_week.change(:year => 2017)
     Planting.where('planting_date_end >= ? AND planting_date_begin <= ?', start_date, end_date)
   end
 
   def self.next_week
-    start_date = Date.today.at_beginning_of_week + 7.days
-    end_date = Date.today.at_end_of_week + 7.days
+    start_date = Date.today.at_beginning_of_week.change(:year => 2017) + 7.days
+    end_date = Date.today.at_end_of_week.change(:year => 2017) + 7.days
     Planting.where('planting_date_end >= ? AND planting_date_begin <= ?', start_date, end_date)
   end
 
   def self.last_week
-    start_date = Date.today.at_beginning_of_week - 7.days
-    end_date = Date.today.at_end_of_week - 7.days
+    start_date = Date.today.at_beginning_of_week.change(:year => 2017) - 7.days
+    end_date = Date.today.at_end_of_week.change(:year => 2017) - 7.days
     Planting.where('planting_date_end >= ? AND planting_date_begin <= ?', start_date, end_date)
   end
 end
